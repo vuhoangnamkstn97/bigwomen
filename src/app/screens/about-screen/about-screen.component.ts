a7b3377d990ac3c966f2f5c5d874e671c9d31b59
vuhoangnamkstn97 <vuhoangnamkstn97@gmail.com>
2022-08-06 17:06:51 +0700

    feat: roadmap for mobile

--- src/app/screens/about-screen/about-screen.component.ts
+++ src/app/screens/about-screen/about-screen.component.ts
@@ -25,6 +25,7 @@ export class AboutScreenComponent implements OnInit, AfterViewInit {
     autoplay: true,
     autoplayTimeout: 2000,
     margin: 20,
+    rtl: false,
     responsive: {
       0: {
         items: 1,
@@ -41,33 +42,148 @@ export class AboutScreenComponent implements OnInit, AfterViewInit {
     },
     nav: true,
   };
-  slidesStore: IOwlImage[] = [];
+  customRevertOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    autoplay: true,
+    autoplayTimeout: 2000,
+    margin: 20,
+    rtl: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
+  slidesStore: IOwlImage[] = [
+    {
+      id: '1',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-1.jpg',
+      title: 'screenshot-1',
+    },
+    {
+      id: '2',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-2.jpg',
+      title: 'screenshot-2',
+    },
+    {
+      id: '3',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-3.jpg',
+      title: 'screenshot-3',
+    },
+    {
+      id: '4',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-4.jpg',
+      title: 'screenshot-4',
+    },
+    {
+      id: '5',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-5.jpg',
+      title: 'screenshot-5',
+    },
+    {
+      id: '6',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-6.jpg',
+      title: 'screenshot-6',
+    },
+  ];
+  revertSlidesStore: IOwlImage[] = [
+    {
+      id: '7',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-7.jpg',
+      title: 'screenshot-7',
+    },
+    {
+      id: '8',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-8.jpg',
+      title: 'screenshot-8',
+    },
+    {
+      id: '9',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-9.jpg',
+      title: 'screenshot-9',
+    },
+    {
+      id: '10',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-10.jpg',
+      title: 'screenshot-10',
+    },
+    {
+      id: '11',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-11.jpg',
+      title: 'screenshot-11',
+    },
+    {
+      id: '12',
+      alt: '',
+      src: 'assets/images/jpg/bigwomen-12.jpg',
+      title: 'screenshot-12',
+    },
+  ];
   ngOnInit(): void {
     console.log('init app about');
     this.slidesStore = [
       {
         id: '1',
         alt: '',
-        src: 'assets/images/jpg/screenshot-1.jpg',
+        src: 'assets/images/jpg/bigwomen-1.jpg',
         title: 'screenshot-1',
       },
       {
         id: '2',
         alt: '',
-        src: 'assets/images/jpg/screenshot-2.jpg',
+        src: 'assets/images/jpg/bigwomen-2.jpg',
         title: 'screenshot-2',
       },
       {
         id: '3',
         alt: '',
-        src: 'assets/images/jpg/screenshot-3.jpg',
+        src: 'assets/images/jpg/bigwomen-3.jpg',
         title: 'screenshot-3',
       },
       {
         id: '4',
         alt: '',
-        src: 'assets/images/jpg/screenshot-4.jpg',
+        src: 'assets/images/jpg/bigwomen-4.jpg',
         title: 'screenshot-4',
+      },
+      {
+        id: '5',
+        alt: '',
+        src: 'assets/images/jpg/bigwomen-5.jpg',
+        title: 'screenshot-5',
+      },
+      {
+        id: '6',
+        alt: '',
+        src: 'assets/images/jpg/bigwomen-6.jpg',
+        title: 'screenshot-6',
       },
     ];
   }
